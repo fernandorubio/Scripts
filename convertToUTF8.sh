f33fea73f59cc4caaf7f2c13e1370766ef84dffe
fernandorubio <fernandorubioblanco@gmail.com>
2018-04-03 14:20:53 +0200

    Update convertToUTF8.sh

--- convertToUTF8.sh
+++ convertToUTF8.sh
@@ -5,14 +5,18 @@ CHAR=""
 FILE=""
 
 UTF="UTF-8"
+#US-ASCII is a UTF-8 subtype.
 ASCII="US-ASCII"
 TEX="text"
 
 
 find . -type f -exec file -bi {} \; -print0 | while read -d $'\0' info
 do
+	#Character encoding
 	CHAR=$( echo $info | awk '{ print toupper($2) }' | cut -d "=" -f 2 )
+	#File type
 	TYPE=$( echo $info | awk '{ print $1 }' | cut -d "/" -f 1 )
+	#Path file
 	FILE=$( echo $info | awk '{ print $3 }')
 		
 	if [ $TYPE == $TEX ]; then
